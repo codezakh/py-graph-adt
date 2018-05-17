4a329a9d4cf0a4767f46c7d56ea5bad3a4f1f928
Zaid Khan <brain@zaidkhan.me>
2018-05-17 04:31:14 -0400

    Added graph adt and made test pass for vtx cnt and adding vertices.

--- graph.py
+++ graph.py
@@ -7,3 +7,14 @@ class Edge:
     def __init__(self, start_vertex, end_vertex):
         self.start = start_vertex
         self.end = end_vertex
+
+class GraphADT:
+    vertices = []
+    edges = []
+
+    @property
+    def vertex_count(self):
+        return len(self.vertices)
+
+    def add_vertex(self, data):
+        self.vertices.append(Vertex(data))
