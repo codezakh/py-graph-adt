132cdc4e841d2adae3f3303c025f3bc5d2b02511
Zaid Khan <brain@zaidkhan.me>
2018-05-18 01:37:46 -0500

    Added test for getting adjacent vertices.

--- tests.py
+++ tests.py
@@ -26,6 +26,14 @@ class GraphTestCase(unittest.TestCase):
         g.add_vertex("second")
         self.assertEqual(g.vertex_count, 2)
 
+    def test_get_adjacent_vertices(self):
+        g = GraphADT()
+        vtx_1 = g.add_vertex("first")
+        vtx_2 = g.add_vertex("second")
+        vtx_3 = g.add_vertex("third")
+        edge = g.add_edge(vtx_1, vtx_2)
+        self.assertSetEqual(vtx_1.incident_edges, set(edge))
+
 
 if __name__ == "__main__":
     unittest.main()
