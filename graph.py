fca4ab3266b151b754181cba79dfa9c90c7d15d2
Zaid Khan <brain@zaidkhan.me>
2018-05-18 01:30:03 -0500

    Added hashability for Edge class to support incident_edges.

--- graph.py
+++ graph.py
@@ -1,12 +1,17 @@
 class Vertex:
     def __init__(self, data):
         self.data = data
+        self.incident_edges = set() 
 
 
 class Edge:
     def __init__(self, start_vertex, end_vertex):
         self.start = start_vertex
         self.end = end_vertex
+        self.end.incident_edges.add(self)
+
+    def __hash__(self):
+        return id(self)
 
 class GraphADT:
     vertices = []
