ee3a77416e54827cb339c59f484b90967e18c8bf
Zaid Khan <brain@zaidkhan.me>
2018-05-18 03:28:55 -0500

    Added test for removing edge.

--- tests.py
+++ tests.py
@@ -35,5 +35,15 @@ class GraphTestCase(unittest.TestCase):
         g.add_edge(vtx1, vtx2)
         self.assertEqual(list(g.get_adjacent(vtx1)), [vtx2])
 
+    def test_removing_edge(self):
+        g = GraphADT()
+        vtx1 = g.add_vertex("first")
+        vtx2 = g.add_vertex("second")
+        edg1 = g.add_edge(vtx1, vtx2)
+        self.assertEqual(g.edge_count, 1)
+        g.remove_edge(vtx1, vtx2)
+        self.assertEqual(g.edge_count, 0)
+        self.assertDictEqual(vtx2.incident_edges, {})
+
 if __name__ == "__main__":
     unittest.main()
