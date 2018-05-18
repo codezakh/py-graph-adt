f3aa4a5583b082c79f919f3fbb9caf51ba6af9d8
Zaid Khan <brain@zaidkhan.me>
2018-05-18 03:29:06 -0500

    Added __repr__ and edge removal methods.

--- graph.py
+++ graph.py
@@ -6,6 +6,9 @@ class Vertex:
     def __hash__(self):
         return id(self)
 
+    def __repr__(self):
+        return 'Vertex("{}")'.format(self.data)
+
 class Edge:
     def __init__(self, start_vertex, end_vertex):
         self.start = start_vertex
@@ -15,13 +18,22 @@ class Edge:
     def __hash__(self):
         return id(self)
 
+    def __repr__(self):
+        return "Edge({src}, {end})".format(src=self.start, end=self.end)
+
 class GraphADT:
-    vertices = set() 
-    edges = set() 
+
+    def __init__(self):
+        self.vertices = set() 
+        self.edges = set() 
 
     @property
     def vertex_count(self):
         return len(self.vertices)
+
+    @property
+    def edge_count(self):
+        return len(self.edges)
 
     def add_vertex(self, data):
         vertex = Vertex(data)
@@ -37,3 +49,14 @@ class GraphADT:
         for edge in self.edges:
             if edge.start is vertex:
                 yield edge.end
+
+    def remove_edge(self, source, endpoint):
+        try:
+            edge = endpoint.incident_edges.pop(source)
+        except KeyError:
+            raise ValueError("Edge {}->{} does not exist".format(
+                source, 
+                endpoint))
+        else:
+            self.edges.remove(edge)
+
